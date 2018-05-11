d85d0a694de40d926c0ef7f21c546d9ea5c6ecf1
OscarLopez352 <ol123321123321@gmail.com>
2018-05-11 11:01:53 -0600

    Commit rir

---
+++ src/routes/moveRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+var moveController = require('../controllers/moveController');
+var ensureToken = require('../middlewares/ensureToken');
+
+module.exports = function (app){
+
+  app.get('/move/listMoves', ensureToken.ensureToken, moveController.listMoves);
+  app.post('/move/createMove', ensureToken.ensureToken, moveController.createMove);
+  app.put('/move/updateMove/:MoveID', ensureToken.ensureToken, moveController.updateMove);
+  app.delete('/move/deleteMove/:MoveID', ensureToken.ensureToken, moveController.deleteMove);
+
+}
