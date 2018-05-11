ccea9fdabc7fa365060c078d09941e8e7367e008
OscarLopez352 <ol123321123321@gmail.com>
2018-05-11 11:03:14 -0600

    Comit 5

---
+++ src/routes/invoiceRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+var invoiceController = require('../controllers/invoiceController');
+var ensureToken = require('../middlewares/ensureToken');
+
+module.exports = function (app){
+
+  app.get('/invoice/listInvoices', ensureToken.ensureToken, invoiceController.listInvoices);
+  app.post('/invoice/createInvoice', ensureToken.ensureToken, invoiceController.createInvoice);
+  app.put('/invoice/updateInvoice/:InvoiceID', ensureToken.ensureToken, invoiceController.updateInvoice);
+  app.delete('/invoice/deleteInvoice/:InvoiceID', ensureToken.ensureToken, invoiceController.deleteInvoice);
+
+}
